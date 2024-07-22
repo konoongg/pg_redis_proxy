d5594ac6b1cee56d92a37094b82cf71b96cfbfde
konoongg <konoongg2004@gmal.com>
2024-07-22 16:11:08 +0700

    add new test

--- t/001_whole_test.pl
+++ t/001_whole_test.pl
@@ -6,7 +6,6 @@ use strict;
 
 # use Socket qw(AF_INET);
 
-
 # This the test file for pg_redis_proxy;
 # It sends RESP queries to default redis socket (127.0.0.1:6379)
 # and then tests correctness of the results.
@@ -32,10 +31,6 @@ my $length = -1;
 my %set_tests = (
 	"*3\r\n\$3\r\nset\r\n\$3\r\nkey\r\n\$5\r\nvalue\r\n" => "+OK\r\n",
 	"*4\r\n\$3\r\nset\r\n\$5\r\nabcde\r\n\$5\r\nefghi\r\n\$5\r\n12345\r\n" => "-ERR syntax error\r\n",
-);
-
-my %get_tests = (
-	
 );
 
 my %del_tests = (
@@ -65,26 +60,38 @@ my %uncorrect_resp_tests = (
 	"*1\r\n\$3\r\fget\r\n\$5value\r\n" => "-ERR illegal according to RESP",
 );
 
-# tests with incorrect commands/incorrect arguments
+#tests with incorrect commands/incorrect arguments
 my %correct_resp_tests = (
-	"*1\r\n\$32\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\n" => "-ERR unknown command 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', with args beginning with: \r\n",
-	"*1\r\n\$5\r\neidkc\r\n" => "-ERR unknown command 'eidkc', with args beginning with: \r\n",
+	"*1\r\n\$32\r\naaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\r\n" => "-ERR unknown command `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa`, with args beginning with: \r\n",
+	"*1\r\n\$5\r\neidkc\r\n" => "-ERR unknown command `eidkc`, with args beginning with: \r\n",
 );
-
+####################################################################################################################################################################################
 # Block 1: basic commands (get/set/del)
+####################################################################################################################################################################################
+# Block 1.1: set tests
+####################################################################################################################################################################################
 
-# Block 1.1: set tests 
-
+#1, 2
 for (keys %set_tests) {
+    $request = "*2\r\n\$3\r\nDEL\r\n\$3\r\nkey\r\n";
+    $socket->send($request);
+    $socket->recv($response, 1024);
+
 	$socket->send($_);
 	$socket->recv($response, 1024);
 	# print("Response: $response, Cur: $_, Hash: $other_tests{$_}\n");
 	ok($response eq $set_tests{$_}, "Set test");
 }
 
+####################################################################################################################################################################################
 # Block 1.2: get tests
-
+####################################################################################################################################################################################
+#3
 # checking its work:
+$request = "*2\r\n\$3\r\nDEL\r\n\$3\r\nkey\r\n";
+$socket->send($request);
+$socket->recv($response, 1024);
+
 $request = "*3\r\n\$3\r\nset\r\n\$3\r\nkey\r\n\$5\r\nvalue\r\n";
 $socket->send($request);
 $socket->recv($response, 1024);
@@ -93,29 +100,55 @@ $request = "*2\r\n\$3\r\nget\r\n\$3\r\nkey\r\n";
 $socket->send($request);
 $socket->recv($response, 1024);
 ok($response eq "\$5\r\nvalue\r\n", "Get test: get works");
+####################################################################################################################################################################################
 
+#4
+#pasring two reqv
+$request = "*2\r\n\$3\r\nDEL\r\n\$3\r\nkey\r\n";
+$socket->send($request);
+$socket->recv($response, 1024);
+
+$request = "*3\r\n\$3\r\nset\r\n\$3\r\nkey\r\n\$5\r\nvalue\r\n";
+$socket->send($request);
+$socket->recv($response, 1024);
+
+$request = "*2\r\n\$3\r\nget\r\n\$3\r\nkey\r\n*2\r\n\$3\r\nget\r\n\$3\r\nkey\r\n";
+$socket->send($request);
+$socket->recv($response, 1024);
+ok($response eq "\$5\r\nvalue\r\n\$5\r\nvalue\r\n", "Get test: get two response");
+####################################################################################################################################################################################
+
+#5
 # "bad" situations
 # Wrong number of arguments
+$request = "*2\r\n\$3\r\nDEL\r\n\$3\r\nkey\r\n";
+$socket->send($request);
+$socket->recv($response, 1024);
 
 $request = "*3\r\n\$3\r\nget\r\n\$3\r\nkey\r\n\$20\r\nUn3x1st1ng_k3y_11251\r\n";
 $socket->send($request);
 $socket->recv($response, 1024);
-ok($response eq "-ERR wrong number of arguments for 'get' command\r\n", "Get test: incorrect number of arguments(2)");
+ok($response eq "-ERR wrong number of arguments for `get` command\r\n", "Get test: incorrect number of arguments(2)");
 
 $request = "*1\r\n\$3\r\nget\r\n";
 $socket->send($request);
 $socket->recv($response, 1024);
 ok($response eq "-ERR wrong number of arguments for 'get' command\r\n", "Get test: incorrect number of arguments(0)");
+####################################################################################################################################################################################
 
+#6
 # trying to get unexisting key:ls
 $request = "*2\r\n\$3\r\nget\r\n\$20\r\nUn3x1st1ng_k3y_11251\r\n";
 $socket->send($request);
 $socket->recv($response, 1024);
 ok($response eq "\$-1\r\n", "Get test: unexisting value");
 
+####################################################################################################################################################################################
 # Block 1.3: del tests
-# del command receives any amount of arguments; keys in arguments are deleted.
+####################################################################################################################################################################################
 
+#7
+# del command receives any amount of arguments; keys in arguments are deleted.
 # filling the table
 for (my $i = 0; $i < 4; $i++) {
 	$request = "*3\r\n\$3\r\nset\r\n\$4\r\nkey$i\r\n\$5\r\nabcdef\r\n";
@@ -136,9 +169,11 @@ for (keys %del_tests) {
 	ok($response eq ":0\r\n", "Del test: on repeating, del doesn't delete keys");
 }
 
-
+####################################################################################################################################################################################
 # Block 2: other commands
+####################################################################################################################################################################################
 
+#8
 for (keys %other_tests) {
 	$socket->send($_);
 	$socket->recv($response, 1024);
@@ -146,11 +181,12 @@ for (keys %other_tests) {
 	ok($response eq $other_tests{$_}, "Other tests");
 }
 
-
+####################################################################################################################################################################################
 # Block 3: sending incorrect data and checking extension for correct failure
-
+####################################################################################################################################################################################
 # Block 3.1: uncorrect according to RESP protocol
-#
+####################################################################################################################################################################################
+
 # ATTENTION
 # All of these tests fail/don't respond even on official server. For this reason, they were commented
 # 
@@ -167,7 +203,7 @@ for (keys %other_tests) {
 for (keys %correct_resp_tests) {
 	$socket->send($_);
 	$socket->recv($response, 1024);
-	# print("Response: $response, Cur: $_, Hash: $correct_resp_tests{$_}\n");
+	print("Response: $response, Cur: $_, Hash: $correct_resp_tests{$_}\n");
 	ok($response eq $correct_resp_tests{$_}, "Uncorrect commands test");
 }
 
